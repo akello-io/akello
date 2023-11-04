7370860ebb736e0ad49978baea7c6742e6930c6a
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2023-11-03 20:32:33 -0400

    Testing new github workflow for python package

--- packages/server/setup.py
+++ packages/server/setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 
-VERSION = '0.0.0.13'
+VERSION = '0.0.0.14'
 DESCRIPTION = 'FastAPI server akello.io'
 LONG_DESCRIPTION = 'FastAPI server for running Measurement Based Care (MBC) programs'
 
