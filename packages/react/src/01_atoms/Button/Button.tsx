bdb8dcbb726502a7be1f0fd434f03c0217b01049
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2023-11-04 23:30:33 -0400

    Enhancing button storybook component and testing deployment (#86)
    * updates
    
    * testing an update to storybook

--- packages/react/src/01_atoms/Button/Button.tsx
+++ packages/react/src/01_atoms/Button/Button.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
+import classNames from "classnames";
 
 export interface ButtonProps {
     label: string;
+    size: 'sm' | 'md' | 'lg';
+    type: 'normal' | 'primary' | 'secondary' | 'accent' | 'ghost' | 'link'
 }
 
 const Button = (props: ButtonProps) => {
-    return <div className={"btn btn-sm btn-primary"}>{props.label}</div>
+
+
+    return <div className={classNames({
+        "btn-sm" : props.size == 'sm',
+        "btn-md" : props.size == 'md',
+        "btn-lg" : props.size == 'lg',
+        "btn-normal" : props.type == 'normal',
+        "btn-primary" : props.type == 'primary',
+        "btn-secondary" : props.type == 'secondary',
+        "btn-accent" : props.type == 'accent',
+        "btn-ghost" : props.type == 'ghost',
+        "btn-link" : props.type == 'link',
+    }, "btn btn-sm btn-primary")}>{props.label}</div>
 };
 
 export default Button;
