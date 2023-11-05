f9c1f0df7ab9c97cd5ce906d9e6480c4bad15b3f
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2023-11-05 12:11:33 -0500

    udpates to react component library

---
+++ packages/react/dist/cjs/03_organisms/RegistryMemberships/RegistryMemberships.stories.d.ts
@@ -0,0 +1,3 @@
+declare const _default: import("@storybook/types").ComponentAnnotations<import("@storybook/react/dist/types-0a347bb9").R, import("./RegistryMemberships").RegistryMembershipsProps>;
+export default _default;
+export declare const BasicExample: import("@storybook/types").AnnotatedStoryFn<import("@storybook/react/dist/types-0a347bb9").R, import("./RegistryMemberships").RegistryMembershipsProps>;
---
+++ packages/react/src/02_molecules/TopNavigation/TopNavigation.stories.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import TopNavigation from './TopNavigation';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+    title: 'Molecules/TopNavigation',
+    component: TopNavigation,
+} as ComponentMeta<typeof TopNavigation>;
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template: ComponentStory<typeof TopNavigation> = (args) => <TopNavigation {...args} />;
+
+export const BasicExample = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+BasicExample.args = {
+    logo: 'https://www.tailorbrands.com/wp-content/uploads/2020/07/mcdonalds-logo.jpg',
+    profile_photo: 'https://www.shutterstock.com/image-photo/profile-picture-smiling-young-african-260nw-1873784920.jpg',
+    classNames: 'bg-black'
+};
