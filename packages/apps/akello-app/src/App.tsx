899e5d18c6bb562630470d9a2e4f1b8c68f27414
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2023-11-05 15:53:42 -0500

    Setting up new App

--- packages/apps/akello-app/src/App.tsx
+++ packages/apps/akello-app/src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import '@aws-amplify/ui-react/styles.css';
+import {Amplify, Auth} from 'aws-amplify';
+import {Authenticator} from "@aws-amplify/ui-react";
+import {cognito_auth_components, cognito_auth_formFields} from "./cognito_auth";
+
+
+Amplify.configure({
+  Auth: {
+    region: process.env.REACT_APP_TEST_AWS_REGION,
+    userPoolId: process.env.REACT_APP_TEST_AWS_COGNITO_USER_POOL_ID,
+    userPoolWebClientId: process.env.REACT_APP_TEST_AWS_COGNITO_USER_POOL_APP_CLIENT_ID
+  }
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Authenticator formFields={cognito_auth_formFields} components={cognito_auth_components} hideSignUp={false}>
+          {({ signOut, user }) => {
+              return (
+                  <>
+                      <button onClick={signOut}>signout</button>
+                      Logged In
+                  </>
+              )
+          }}
+      </Authenticator>
     </div>
   );
 }
