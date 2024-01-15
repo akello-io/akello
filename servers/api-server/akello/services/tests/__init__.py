387b650c3bf74bec32a88da36b35d1310254428a
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-01-15 03:39:30 -0800

    + refactor out mock env configs

---
+++ servers/api-server/akello/services/tests/__init__.py
@@ -0,0 +1,20 @@
+
+mock_env_configs = {
+    "AKELLO_API_URL": "TRUE",
+    "AKELLO_UNIT_TEST": "TRUE",
+    "AKELLO_COGNITO_LOCAL": "TRUE",
+    "AKELLO_COGNITO_URL": "http://localhost:9229",
+    "AKELLO_DYNAMODB_LOCAL": "TRUE",
+    "AKELLO_DYNAMODB_LOCAL_URL": "=http://localhost:8001",
+    "AWS_ACCESS_KEY_ID": "UnitTestLocalDB",
+    "AWS_ACCOUNT_ID": "x",
+    "AWS_BUCKET": "x",
+    "AWS_CLOUD_FRONT_DISTRIBUTION": "x",
+    "AWS_COGNITO_APP_CLIENT_ID": "x",
+    "AWS_COGNITO_USERPOOL_ID": "x",
+    "AWS_DYNAMODB_TABLE": "x",
+    "AWS_REGION": "x",
+    "AWS_SECRET_ACCESS_KEY": "x",
+    "AWS_SECRET_NAME": "x",
+    "AWS_STORYBOOK_BUCKET": "x",
+}
--- servers/api-server/akello/services/tests/test_user_service.py
+++ servers/api-server/akello/services/tests/test_user_service.py
@@ -1,26 +1,9 @@
 import os
 from unittest import TestCase, mock
 from unittest.mock import patch
+from akello.services.tests import mock_env_configs
 
-@mock.patch.dict(os.environ, {
-    "AKELLO_API_URL": "TRUE",
-    "AKELLO_UNIT_TEST": "TRUE",
-    "AKELLO_COGNITO_LOCAL": "TRUE",
-    "AKELLO_COGNITO_URL": "http://localhost:9229",
-    "AKELLO_DYNAMODB_LOCAL": "TRUE",
-    "AKELLO_DYNAMODB_LOCAL_URL": "=http://localhost:8001",
-    "AWS_ACCESS_KEY_ID": "UnitTestLocalDB",
-    "AWS_ACCOUNT_ID": "x",
-    "AWS_BUCKET": "x",
-    "AWS_CLOUD_FRONT_DISTRIBUTION": "x",
-    "AWS_COGNITO_APP_CLIENT_ID": "x",
-    "AWS_COGNITO_USERPOOL_ID": "x",
-    "AWS_DYNAMODB_TABLE": "x",
-    "AWS_REGION": "x",
-    "AWS_SECRET_ACCESS_KEY": "x",
-    "AWS_SECRET_NAME": "x",
-    "AWS_STORYBOOK_BUCKET": "x",
-})
+@mock.patch.dict(os.environ, mock_env_configs)
 class TestUserService(TestCase):
 
     @patch('akello.dynamodb.registry_db')
