927aac1bbfa623c65e628f2de46177a2c23140a2
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-01-15 16:23:07 -0800

    testing github workflow

--- servers/api-server/akello/settings.py
+++ servers/api-server/akello/settings.py
@@ -23,7 +23,8 @@ def set_aws():
     secrets = get_secret()
     setenvars(secrets)
 
-if os.getenv('AKELLO_COGNITO_LOCAL') == 'TRUE' or os.getenv('AKELLO_COGNITO_CI') == 'True':
+# use os.environ to force an exception if it doesn't exist
+if os.environ['AKELLO_COGNITO_LOCAL'] == 'TRUE':
     set_local()
 else:
     set_aws()
