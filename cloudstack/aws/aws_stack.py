b11fc1054e3e398b28f11120acbeb08ad46f1aa5
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-01-17 09:38:21 -0800

    init cdk scripts

--- cloudstack/aws/aws_stack.py
+++ cloudstack/aws/aws_stack.py
@@ -1,19 +1,21 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
+from aws_cdk import (    
+    Stack
 )
 from constructs import Construct
+from aws.components.storage import S3
+from aws.components.storage import DynamoDB
+
+
 
 class AwsStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        # Setup all the data stores we need
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "AwsQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        DynamoDB(self, 'akellodb', table_name='registry', partition_key='pk', sort_key='sk')
+        S3(self, 'akello.io', bucket_name='akello.io')
+        S3(self, 'app.akello.io', bucket_name='app.akello.io')
+        S3(self, 'docs.akello.io', bucket_name='docs.akello.io')
+
---
+++ cloudstack/aws/components/storage.py
@@ -0,0 +1,71 @@
+import aws_cdk as cdk
+from constructs import Construct
+from aws_cdk import (    
+    aws_s3 as s3,
+    aws_s3_notifications,
+    aws_dynamodb as dynamodb
+)
+from aws_cdk import aws_healthlake as healthlake
+
+
+class S3(Construct):
+    def __init__(
+        self, scope: Construct, id_: str, *, bucket_name: str, lambda_notification = None
+    ):
+        super().__init__(scope, id_)
+        
+        self.bucket = s3.Bucket(
+            self,            
+            bucket_name,
+            bucket_name=bucket_name,
+            versioned=False,
+            access_control=s3.BucketAccessControl.PRIVATE,
+            removal_policy=cdk.RemovalPolicy.DESTROY,
+            encryption=s3.BucketEncryption.KMS_MANAGED,    
+            bucket_key_enabled=True        
+        )
+        
+        if lambda_notification:
+            self.bucket.add_event_notification(
+                s3.EventType.OBJECT_CREATED, 
+                lambda_notification,
+                s3.NotificationKeyFilter(                      
+                  suffix=".json",
+                )
+            )
+
+class DynamoDB(Construct):
+    def __init__(
+        self, scope: Construct, id_: str, *, table_name: str, partition_key: str, sort_key: str = None
+    ):
+        super().__init__(scope, id_)
+        table = dynamodb.TableV2(self, 'Table',
+                table_name=table_name,
+                partition_key=dynamodb.Attribute(name=partition_key, type=dynamodb.AttributeType.STRING),
+                sort_key=dynamodb.Attribute(name=sort_key, type=dynamodb.AttributeType.STRING), 
+                contributor_insights=True,
+                table_class=dynamodb.TableClass.STANDARD_INFREQUENT_ACCESS,
+                point_in_time_recovery=True
+            )        
+
+class Healthlake(Construct):
+    def __init__(
+        self, scope: Construct, id_: str, *, healthlake_db_name: str
+    ):
+        super().__init__(scope, id_)
+        
+        self.cfn_fHIRDatastore = healthlake.CfnFHIRDatastore(
+            self, 
+            healthlake_db_name,
+            identity_provider_configuration=healthlake.CfnFHIRDatastore.IdentityProviderConfigurationProperty(
+                authorization_strategy="SMART_ON_FHIR_V1",
+        
+                # the properties below are optional
+                fine_grained_authorization_enabled=False,
+                idp_lambda_arn="idpLambdaArn",
+                metadata="metadata"
+            ),
+            datastore_type_version="R4", 
+            datastore_name=healthlake_db_name
+        )
+        self.cfn_fHIRDatastore.apply_removal_policy(cdk.RemovalPolicy.DESTROY)        
