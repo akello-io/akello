5b1957fbe7b68373c1494a7801b03a5b76256c4d
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-01-24 11:05:32 -0800

    fixing docker configs and npm start performance issue

--- apps/cocm-registry/tailwind.config.js
+++ apps/cocm-registry/tailwind.config.js
@@ -1,12 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/react-tailwindcss-datepicker/dist/index.esm.js",
-    "node_modules/daisyui/dist/**/*.js",
-    "node_modules/react-daisyui/dist/**/*.js",
-
-  ],
+  content: ["./src/**/*.{html,js,tsx}"],
+  daisyui: {
+    themes: ["light", "dark", "cupcake"],
+  },
   theme: {
     fontFamily: {
       display: ["Satisfy", "cursive"],
