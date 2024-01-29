65c36f1478e34f3f66c2fada3c5298768d589f9d
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-01-29 06:53:58 -0800

    fixing packages

--- packages/react/.storybook/main.js
+++ packages/react/.storybook/main.js
@@ -2,11 +2,38 @@
 const config = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
-    "@storybook/addon-interactions",
-  ],
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@storybook/addon-interactions',
+    '@storybook/addon-mdx-gfm',
+    {
+        name: '@storybook/addon-styling-webpack',
+        options: {
+            rules: [
+                {
+                    test: /\.css$/,
+                    sideEffects: true,
+                    use: [
+                        require.resolve('style-loader'),
+                        {
+                            loader: require.resolve('css-loader'),
+                            options: {
+                                importLoaders: 1,
+                            },
+                        },
+                        {
+                            loader: require.resolve('postcss-loader'),
+                            options: {
+                                implementation: require.resolve('postcss'),
+                            },
+                        },
+                    ],
+                },
+            ],
+        },
+    },
+],  
   framework: {
     name: "@storybook/react-vite",
     options: {},
