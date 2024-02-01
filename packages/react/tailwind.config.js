69e5f21e2d9fc2585e79ef5b6168dee229230e84
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-02-01 10:49:02 -0800

    working fix

--- packages/react/tailwind.config.js
+++ packages/react/tailwind.config.js
@@ -1,8 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts,tsx}'],
+  content: [
+    './src/**/*.{ts,tsx}',
+    'node_modules/daisyui/src/**/*.{ts,tsx}'
+  ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  daisyui: {
+    styled: true,
+    themes: true,
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+  plugins: [require("daisyui")]
 }
