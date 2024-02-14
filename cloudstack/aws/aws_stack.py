479e6cb516575cdeb8707dbdef90d71ef1d4408d
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-02-13 17:19:25 -0800

    aws cdk with dynamodb and lambda working

--- cloudstack/aws/aws_stack.py
+++ cloudstack/aws/aws_stack.py
@@ -22,8 +22,9 @@ class AwsStack(Stack):
         # R53(self, 'r53_akello.io', name='app.akello.io', zone_name='akello.io')
 
         # Setup data stores
-        DynamoDB(self, 'dynamo_akellodb', table_name='akello-multi-tenant', partition_key='pk', sort_key='sk')
+        DynamoDB(self, 'dynamo_akellodb', table_name='akello-multi-tenant', partition_key='partition_key', sort_key='sort_key')
     
+        # Setup Storage
         S3(self, 's3_app.akello.io', bucket_name='app.akello.io')
                
 
@@ -31,8 +32,7 @@ class AwsStack(Stack):
         cognito_pool = CognitoPool(self, 'cognito_akello', name='akello', user_pool_name='akello')
         cognito_client = CognitoClient(self, 'cognito_client_akello', user_pool=cognito_pool.user_pool, name='app-client')        
 
-        
-        
+        # Setup Lambda        
         fn_Lambda(
             self, 
             'fn_akello_api', 
@@ -40,6 +40,7 @@ class AwsStack(Stack):
             path='../servers/api-server', 
             dockerfile='Dockerfile.aws.lambda', 
             lambda_env={
+                'AWS_DYNAMODB_TABLE': 'akello-multi-tenant',
                 'AWS_COGNITO_USERPOOL_ID': cognito_pool.user_pool.user_pool_id,
                 'AWS_COGNITO_APP_CLIENT_ID': cognito_client.client.user_pool_client_id
             }
