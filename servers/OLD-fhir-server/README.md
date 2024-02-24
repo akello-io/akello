17ba71ccc56b385c9e980197f6d95ffbac3caa5c
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-02-23 21:18:29 -0800

    clearing out old FHIR server

--- servers/OLD-fhir-server/README.md
+++
@@ -1,7 +0,0 @@
-
-```commandline
-python3 -m venv venv 
-source venv/bin/activate
-pip install -r requirements.txt
-uvicorn server.main:app --reload
-```
--- servers/OLD-fhir-server/server/api/v1/api.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from .endpoints import fhir
-
-
-router = APIRouter()
-router.include_router(fhir.router, prefix="/fhir", tags=["FHIR"])
-
