5805d7b889af1848f424d6451387066d2ffd2f09
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-02-24 08:58:39 -0800

    cleaning up webhook

--- servers/api-server/akello/plugins/metriport/webhook.py
+++ servers/api-server/akello/plugins/metriport/webhook.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter
-from akello.services.registry import RegistryService
-from akello.db.models import PatientRegistry, EventLog
-import uuid
-import logging
-import datetime 
 import json
+import logging
+
+from fastapi import APIRouter
+
+from akello.db.models import PatientRegistry, EventLog
+from akello.services.registry import RegistryService
 
 
 logger = logging.getLogger('mangum')
@@ -20,12 +20,12 @@ async def metriport_webhook(payload: dict):
     if 'data' not in payload['meta']:
         print("No data in payload")
         return
-    
+  
     mixin = payload['meta']['data']['mixin']
     registry_id = payload['meta']['data']['registry_id']
 
 
-    if mixin == 'MetriportPatientSessionTreatmentLog':                
+    if mixin == 'MetriportPatientSessionTreatmentLog':
         patient_mrn = payload['meta']['data']['patient_mrn']
         treatment_log_id = payload['meta']['data']['treatment_log_id']
         patient = RegistryService.get_patient(registry_id, patient_mrn)
@@ -40,30 +40,3 @@ async def metriport_webhook(payload: dict):
             if treatment_log.id == treatment_log_id:
                 treatment_log.scores = scores
         RegistryService.update_patient(patient)
-                
-
-    """
-    if payload['meta']['type'] == 'medical.consolidated-data':
-        registry_id = payload['meta']['data']['registry_id']
-        for patient in payload['patients']:                                       
-            saved_patient = RegistryService.get_patient(registry_id, patient['patientId'])
-
-            if saved_patient is None:
-                #TODO: Raise an exception here
-                print("Patient not found in registry")                
-                continue
-
-            patient_registry = PatientRegistry(**saved_patient)
-            event_log = EventLog(
-                id=str(uuid.uuid4()),
-                system='metriport',
-                data=payload,
-                created_date=datetime.datetime.now().timestamp(),
-                modified_date=datetime.datetime.now().timestamp()
-            )
-            patient_registry.event_logs.append(event_log)
-                
-            # patient_registry.integration_metriport_fhir_data = patient # TODO: only for debugging
-            RegistryService.update_patient(patient_registry)
-            print("---------------------METRIPORT WEBHOOK COMPELTED!!--------------------------")        
-    """
