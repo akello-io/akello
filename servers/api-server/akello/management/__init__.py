79360f81a98be89113534964a99e36eb523a4a08
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-02-25 15:08:30 -0800

    bug fixes to data generation

--- servers/api-server/akello/management/__init__.py
+++ servers/api-server/akello/management/__init__.py
@@ -44,12 +44,20 @@ if __name__ == "__main__":
 
 """
 
+
+
+
+
+
+
+
 import os, random
 from akello.management.commands.mock.registry import RegistryMock
 from akello.management.commands.mock.user import UserMock
 from akello.management.commands.mock.patient import PatientMock
 from akello.services.screeners import ScreenerService
 from akello.services.user import UserService
+from akello.services.registry import RegistryService
 from akello.db.connector.dynamodb import client, dynamodb
 from akello.db.models import UserInvite, UserRole, UserRegistry, ContactTypes
 
@@ -101,21 +109,23 @@ UserService.create_registry_user(registry_id=registry_id, first_name='Vijay2', l
 UserService.create_registry_user(registry_id=registry_id, first_name='Vijay3', last_name='Selvaraj', email='v3@t.com', user_id='1', role=UserRole.care_manager, is_admin=False)
 
 
+for b in range(100):
+    pm = PatientMock(registry_id=registry_id)
+    registry.refer_patient(pm.patient_registry)
+    for treatment_log in pm.treatment_logs:
+        print(len(pm.treatment_logs))
+        RegistryService.add_treatment_log(pm.patient_registry.id, pm.patient_registry.patient_mrn, treatment_log)
 
 
+"""
 for b in range(100):
     mock_patient = PatientMock().create_patient(registry_id)
     registry.refer_patient(mock_patient)
 
-    """
-    initial_assessment = 'Initial Assessment'
-    follow_up = 'Follow Up'
-    psychiatric_consultation = 'Psychiatric Consultation'
-    relapse_prevention = 'Relapse Prevention Plan'
-    """
 
     registry.add_treatment_log(patient_registry=mock_patient, contact_type=ContactTypes.initial_assessment,  weeks_in_treatment=0)
     for i in range(1, 8):
         random_contact_type = random.choices(population=[ContactTypes.follow_up, ContactTypes.psychiatric_consultation], weights=[0.9, 0.1], k=1)[0]
         registry.add_treatment_log(patient_registry=mock_patient, contact_type=random_contact_type,  weeks_in_treatment=i)
     registry.add_treatment_log(patient_registry=mock_patient, contact_type=ContactTypes.relapse_prevention,  weeks_in_treatment=i)    
+"""
