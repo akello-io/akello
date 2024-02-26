1b513111a1754428a2a31eb68e3ae1f9bb22e10d
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-02-26 12:22:18 -0800

    initial configurations for akello plugin architecture

--- servers/api-server/akello/plugins/mixin_base.py
+++ servers/api-server/akello/plugins/mixin_base.py
@@ -1,5 +1,37 @@
+from enum import Enum, IntEnum
+from pydantic import BaseModel
 
-class PatientMixinBase:
+
+class MixinType(str, Enum):
+    pre_webhook = 'Pre Webhook'
+    post_webhook = 'Post Webhook'
+
+
+
+class BaseMixin(BaseModel):
+
+    type: str
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+
+class PreWebHookMixinBase(BaseMixin):
+
+    type: MixinType = MixinType.pre_webhook
+
+    def run(self, data):
+        raise NotImplementedError('run() must be implemented in a subclass')
+    
+
+class PostWebHookMixinBase(BaseMixin):
+
+    type: MixinType = MixinType.post_webhook
+    
+    def run(self, data):
+        raise NotImplementedError('run() must be implemented in a subclass')
+
+class PatientMixinBase(BaseMixin):
 
     def run(self, patient):
         raise NotImplementedError('run() must be implemented in a subclass')
