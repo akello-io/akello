02e494a610a30d2006ba4e605507823153617ff1
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-03-22 09:01:53 -0700

    cleaning up pricing, small bug fixes

--- packages/react/src/lib/components/molecules/core/auth2/auth-sign-in/index.tsx
+++ packages/react/src/lib/components/molecules/core/auth2/auth-sign-in/index.tsx
@@ -1,8 +1,10 @@
 import { useState } from 'react';
-import { Checkbox, Anchor, Paper, Title, Text, Container, Group, Button, Center } from '@mantine/core';
+import { Checkbox, Box, LoadingOverlay, Anchor, Paper, Title, Text, Container, Group, Button, Center } from '@mantine/core';
 import { Field, Form, Formik } from 'formik';
 import * as Yup from 'yup';
 import { useAkello } from '@akello/react-hook';
+
+import { useDisclosure } from '@mantine/hooks';
 
 
 interface LoginPageProps {
@@ -15,6 +17,8 @@ interface LoginPageProps {
 export const LoginPage:React.FC<LoginPageProps> = ({onSuccess, onFail, onSignupClick, onForgotPasswordClick}) => {    
     const akello = useAkello();
     const [error, setError] = useState<string | null>(null);
+    const [visible, { toggle }] = useDisclosure(false);
+
 
     const LoginSchema = Yup.object().shape({
         email: Yup.string().email('Invalid email').required('Required'),
@@ -22,19 +26,31 @@ export const LoginPage:React.FC<LoginPageProps> = ({onSuccess, onFail, onSignupC
     });
 
     const handleSubmit = (values: { email: string; password: string }) => {        
+        toggle();        
         akello.login(
             values.email,
             values.password,
             (token: string) => {  
                 akello.setUserName(values.email);                  
-                onSuccess && onSuccess(token);                
+                onSuccess && onSuccess(token);
+                toggle();
             },
             (err: any) => {
                 setError(err.message);
                 onFail && onFail(err);
+                toggle();
             }
         );
     };
+
+
+    const handleOnKeyDown = (e: any) => {
+        if (e.key === 'Enter') {
+            e.preventDefault();
+            e.stopPropagation();
+            return false;
+        }
+    }
 
     return (
         <Formik
@@ -47,6 +63,10 @@ export const LoginPage:React.FC<LoginPageProps> = ({onSuccess, onFail, onSignupC
         >
             {({ errors, touched }) => (
                 <Form>
+                    <Box pos="relative">
+                        <LoadingOverlay visible={visible} zIndex={1000} overlayProps={{ radius: "sm", blur: 2 }} />
+                        {/* ...other content */}
+                    </Box>
                     <div className="w-screen">
                         <Center>
                             <Container size={420} my={40}>
@@ -65,6 +85,7 @@ export const LoginPage:React.FC<LoginPageProps> = ({onSuccess, onFail, onSignupC
                                         name="email"
                                         placeholder="Enter your email address"
                                         className="input input-bordered bg-white w-full"
+                                        onKeyDown={handleOnKeyDown}
                                     />
                                     <div className="label text-error">
                                         {errors.email && touched.email ? <div>{errors.email}</div> : null}
@@ -74,6 +95,7 @@ export const LoginPage:React.FC<LoginPageProps> = ({onSuccess, onFail, onSignupC
                                         name="password"
                                         placeholder="Enter your password"
                                         className="input input-bordered bg-white w-full"
+                                        onKeyDown={handleOnKeyDown}
                                     />
                                     <div className="label text-error">
                                         {errors.password && touched.password ? <div>{errors.password}</div> : null}
