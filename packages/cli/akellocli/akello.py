48546aaa097fff49c5a6542f879c621a2f95dee1
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-03-25 14:56:57 -0700

    + update to CLI to get registry statistics

--- packages/cli/akellocli/akello.py
+++ packages/cli/akellocli/akello.py
@@ -1,4 +1,15 @@
-from akellocli.commands.local import local_commands
+import click
+from akellocli.commands.setup import setup
+from akellocli.commands.start import start
+from akellocli.commands.analytics import analytics
+
+@click.group()
+def local_commands():
+    pass
+
+local_commands.add_command(setup)
+local_commands.add_command(start)
+local_commands.add_command(analytics)
 
 
 def cli():
