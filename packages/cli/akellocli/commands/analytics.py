ed73fba9a6efb822b052225f60d573620bc71e6b
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-03-27 12:47:16 -0700

    Analytics export for members

--- packages/cli/akellocli/commands/analytics.py
+++ packages/cli/akellocli/commands/analytics.py
@@ -2,6 +2,7 @@
 import click
 import pathlib
 from boto3 import Session
+import boto3
 from boto3.dynamodb.conditions import Key
 from datetime import datetime
 
@@ -23,11 +24,13 @@ class bcolors:
 @click.command()
 @click.option('--region', required=True, help='AWS region.')
 @click.option('--profile', required=True, help='AWS profile to use.')
+@click.option('--userpool-id', required=True, help='AWS cognito user-pool')
 @click.option('--dynamodb-table', required=True, help='DynamoDB Table used to analyze.')
-def analytics(region, profile, dynamodb_table):    
+def analytics(region, profile, userpool_id, dynamodb_table):    
     """Export analytics from the accounts to trak engagement and product metrics."""
     session = Session(profile_name=profile)
     db = session.resource('dynamodb', region_name=region)
+    cognito = session.client('cognito-idp')
     tables = list(db.tables.all())
     
     print(bcolors.OKCYAN + "------------------------------" + bcolors.ENDC)
@@ -36,23 +39,13 @@ def analytics(region, profile, dynamodb_table):
         print(bcolors.OKBLUE + f"{idx + 1}. {table.table_name}" + bcolors.ENDC)    
     print(bcolors.OKCYAN + "------------------------------" + bcolors.ENDC)
     print(bcolors.OKGREEN + f"You selected to run an anlytics request on the table {dynamodb_table}" + bcolors.ENDC)        
-
-    """
-    value = click.prompt('Is this correct? (yes/no)', type=str)
-    if value == 'no':
-        return
-    
-    with click.progressbar([1, 2, 3]) as bar:
-        for x in bar:
-            print(f"building report({x})...")
-            time.sleep(x)
-    """
         
     print(bcolors.OKGREEN + f"Report generated successfully" + bcolors.ENDC)        
 
     registry_ids = scan_registeries(db.Table(dynamodb_table))
+    print('registry_id,first_name, last_name, email, last_login, date_refered, patients_refered')
     for registry_id in registry_ids:        
-        scan_patients(db.Table(dynamodb_table), registry_id.split(':')[1])
+        scan_patients(cognito, userpool_id, db.Table(dynamodb_table), registry_id.split(':')[1])
 
 
 def scan_registeries(db_table):    
@@ -78,7 +71,7 @@ def get_user(db_table, user_id):
     )
     return response['Item']
 
-def scan_patients(db_table, registry_id):
+def scan_patients(cognito, userpool_id, db_table, registry_id):
     partition_key = f'registry-patient:{registry_id}'
 
     response = db_table.scan(
@@ -86,27 +79,48 @@ def scan_patients(db_table, registry_id):
     )
     users = get_users_in_registry(db_table, registry_id)    
     
+    
+    
     user_emails = []
     for user in users:        
         user_object = get_user(db_table, user['user_id'])
+        try:  
+            user_profile = cognito.admin_get_user(
+                UserPoolId=userpool_id,
+                Username=user_object['email']            
+            )
+            given_name = user_profile['UserAttributes'][2]['Value']
+            family_name = user_profile['UserAttributes'][3]['Value']
+        except Exception as e:
+            given_name = ''
+            family_name = ''
+
         user_emails.append(
             {
-                'email': user_object['email'], 
-                'last_login': datetime.strftime(datetime.fromtimestamp(int(user_object['last_login'])), '%d/%b/%y')
+                'email': user_object['email'],
+                'given_name': given_name,
+                'family_name': family_name,
+                'date_created': datetime.strftime(datetime.fromtimestamp(int(user_object['date_created'])), '%m-%d-%y'),
+                'last_login': datetime.strftime(datetime.fromtimestamp(int(user_object['last_login'])), '%m-%d-%y')
             }
-        )
+        )                
 
     patient_created_stats = {}
     for patient in response['Items']:
-        date_created = datetime.strftime(datetime.fromtimestamp(int(patient['date_created'])), '%d/%b/%y')         
+        date_created = datetime.strftime(datetime.fromtimestamp(int(patient['date_created'])), '%m-%d-%y')         
         if date_created in patient_created_stats:
             patient_created_stats[date_created] += 1
         else:
             patient_created_stats[date_created] = 1
 
-    print ({
-        'registry_id': registry_id,
-        'emails': user_emails,        
-        'patient_created_stats': patient_created_stats
-    })
+    
+    
+    if len(patient_created_stats.items()) == 0:
+        for user_email in user_emails:
+                print(f"{registry_id},{user_email['given_name']},{user_email['family_name']},{user_email['email']},{user_email['last_login']},,")        
+        
+    else:        
+        for key, value in patient_created_stats.items():
+            for user_email in user_emails:
+                print(f"{registry_id},{user_email['given_name']},{user_email['family_name']},{user_email['email']},{user_email['last_login']}, {key}, {value}")        
         
