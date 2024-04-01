b6387f120c84540f87cc42a34b9e269f5f7e2a3c
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-04-01 15:29:46 -0700

    + login component

---
+++ packages/react/src/lib/components/templates/pages/login-page/index.tsx
@@ -0,0 +1,21 @@
+import { useNavigate } from 'react-router';
+import { LoginPage as LoginPageComponent } from '../../../molecules';
+
+const LoginPage = () => {
+    const navigate = useNavigate();
+
+
+
+    return (
+        <LoginPageComponent
+            onSuccess={(token: string) => {
+                console.log('token', token);
+                navigate('/');
+            }}
+            onSignupClick={() => navigate('/signup')}
+            onForgotPasswordClick={() => navigate('/forgot-password')}
+        />
+    );
+};
+
+export default LoginPage;
