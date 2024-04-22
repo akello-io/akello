87de005e02b6fcb1d5b7d47413947fa44a6947d9
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-04-21 22:00:49 -0700

    working version of docker container deployment via cdk

--- microservices/user/infra/cdk/cdk_stack.py
+++ microservices/user/infra/cdk/cdk_stack.py
@@ -1,19 +1,45 @@
+import aws_cdk as cdk
 from aws_cdk import (
     # Duration,
     Stack,
-    # aws_sqs as sqs,
+    aws_apigateway as apigateway,
 )
 from constructs import Construct
+from aws_cdk import (
+    aws_lambda as _lambda,
+    aws_logs as logs
+)
 
 class CdkStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
+        test_fn = _lambda.DockerImageFunction(
+            scope=self,
+            architecture=_lambda.Architecture.ARM_64,
+            timeout=cdk.Duration.seconds(15),
+            id='test-lambda',
+            # Function name on AWS
+            function_name='test-lambda',
+            # Use aws_cdk.aws_lambda.DockerImageCode.from_image_asset to build
+            # a docker image on deployment
+            code=_lambda.DockerImageCode.from_image_asset(
+                # Directory relative to where you execute cdk deploy
+                # contains a Dockerfile with build instructions
+                directory='../user',
+                file='Dockerfile.aws.lambda'
+            )
+        )
 
-        # example resource
-        # queue = sqs.Queue(
-        #     self, "CdkQueue",
-        #     visibility_timeout=Duration.seconds(300),
-        # )
+        # Define the API Gateway resource
+        api = apigateway.LambdaRestApi(
+            self,
+            "HelloWorldApi",
+            handler = test_fn,
+            proxy = False,
+        )
+
+        # Define the '/hello' resource with a GET method
+        hello_resource = api.root.add_resource("hello")
+        hello_resource.add_method("GET")
--- microservices/user/user/entrypoints/api/main.py
+++ microservices/user/user/entrypoints/api/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from mangum import Mangum
 
 app = FastAPI()
 
@@ -6,3 +7,6 @@ app = FastAPI()
 @app.get("/")
 async def root():
     return {"message": "Hello User Service!"}
+
+handler = Mangum(app)
+
