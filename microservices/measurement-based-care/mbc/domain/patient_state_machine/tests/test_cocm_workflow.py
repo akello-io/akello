caecd1576544727298ea68f4d275ae668cab38ec
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-04-27 23:00:45 -0700

    + wip

--- microservices/measurement-based-care/mbc/domain/patient_state_machine/tests/test_cocm_workflow.py
+++ microservices/measurement-based-care/mbc/domain/patient_state_machine/tests/test_cocm_workflow.py
@@ -3,67 +3,21 @@ from mbc.domain.model.patient import Patient
 from mbc.domain.patient_state_machine.patient_state_machine import CoCMPatientStateMachine
 from mbc.domain.patient_state_machine.utils import build_state
 
-config = {
-    'states': {
-        'referred': {
-            'prerequisites': ['has_consent_condition'],
-            'event_functions': ['flag_event']
-        },
-        'treatment': {
-            'prerequisites': ['has_consent_condition', 'has_moderate_depression'],
-            'event_functions': ['flag_event'],
-            'callbacks': [{
-                'event': 'enter',
-                'fn': 'on_enter_treatment'
-            }]
-        },
-        'relapse_prevention': {
-            'prerequisites': [],
-            'event_functions': []
-        },
-        'discharged': {
-            'prerequisites': [],
-            'event_functions': []
-        }
-    },
-    'transitions': [
-        {
-            'trigger': 'accept',
-            'source': 'referred',
-            'target': 'treatment',
-            'conditions': ['check_transition']
-        },
-        {
-            'trigger': 'flag',
-            'source': 'treatment',
-            'target': 'treatment',
-        },
-        {
-            'trigger': 'billable',
-            'source': 'treatment',
-            'target': 'treatment',
-        },
-        {
-            'trigger': 'graduate',
-            'source': 'treatment',
-            'target': 'relapse_prevention',
-            'conditions': ['check_transition']
-        },
-        {
-            'trigger': 'discharge',
-            'source': '*',
-            'target': 'discharged',
-        }
-    ]
-}
+import yaml
 
-states = [
-    build_state(
+with open('./mbc/domain/patient_state_machine/tests/data/cocm_config.yml') as f:
+    config = yaml.safe_load(f)
+    config = config['workflow']
+
+states = []
+for state in config['states']:
+    states.append(build_state(
         state,
         prerequisites=config['states'][state]['prerequisites'],
         event_functions=config['states'][state]['event_functions'],
         callbacks=config['states'][state].get('callbacks', [])
-    ) for state in config['states']]
+    ))
+
 machine = CoCMPatientStateMachine(
     Patient(registry_id='123', user_id='456', created_at=3),
     DynamoDBPatientQueryService(),
