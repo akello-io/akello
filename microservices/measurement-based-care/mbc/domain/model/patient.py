26b11d96877791a987175a033b4de9bc7976865d
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-04-28 20:50:20 -0700

    + wip

--- microservices/measurement-based-care/mbc/domain/model/patient.py
+++ microservices/measurement-based-care/mbc/domain/model/patient.py
@@ -1,5 +1,7 @@
 from pydantic import BaseModel
+
 from mbc.domain.model.measurement import Measurement
+
 
 class Patient(BaseModel):
     registry_id: str
@@ -13,4 +15,7 @@ class Patient(BaseModel):
 
     baseline_measurements: dict[str, Measurement] = {}
     latest_measurements: dict[str, Measurement] = {}
-    dates_of_interaction: dict[str, float] =  {}
+    dates_of_interaction: dict[str, float] = {}
+
+    def billable(self, type: str, minutes: int):
+        print("save billable event")
