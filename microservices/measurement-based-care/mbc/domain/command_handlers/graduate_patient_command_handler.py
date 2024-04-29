48f4a86a046fa89892124694e88c4a30b6623c1a
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-04-28 21:57:01 -0700

    + wip

--- microservices/measurement-based-care/mbc/domain/command_handlers/graduate_patient_command_handler.py
+++ microservices/measurement-based-care/mbc/domain/command_handlers/graduate_patient_command_handler.py
@@ -1,14 +1,12 @@
-import uuid
-from datetime import datetime, timezone
-
 from mbc.domain.commands import graduate_patient_command
-from mbc.adapters.dynamodb_query_service import dynamodb_query_service
+from mbc.adapters.dynamodb_query_service import PatientQueryService
+from mbc.domain.state_machines.patient_machine.machine import PatientStateMachine
 
 
 def handle_create_user_command(
     command: graduate_patient_command.GraduatePatientCommand,
-    user_query_service: dynamodb_query_service.PatientQueryService,
+    user_query_service: PatientQueryService,
 ) -> str:
-    current_time = datetime.now(timezone.utc).isoformat()
-
+    machine = PatientStateMachine(command.patient, user_query_service)
+    machine.graduate()
 
