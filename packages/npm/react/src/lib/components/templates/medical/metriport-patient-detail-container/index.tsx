0772cdefd97f360e6e390da1ddde167334cdbcf0
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-05-09 22:20:21 -0700

    + update

--- packages/npm/react/src/lib/components/templates/medical/metriport-patient-detail-container/index.tsx
+++ packages/npm/react/src/lib/components/templates/medical/metriport-patient-detail-container/index.tsx
@@ -10,6 +10,7 @@ import { PatientSessionStepper } from "../../../molecules";
 import { PatientTimeLog } from "../../../organisms/medical";
 import { MeasureTypes } from "@akello/core";
 import { PatientProgress } from "../../../organisms/medical/patient-progress";
+import { LineChart } from "../../../atoms/insights/line-chart";
 
 export interface MetriportPatientDetailContainerProps {
     selectedPatient: RegistryTreatment
@@ -19,9 +20,14 @@ export interface MetriportPatientDetailContainerProps {
 export const MetriportPatientDetailContainer: React.FC<MetriportPatientDetailContainerProps> = ({selectedPatient, registry_id}) => {
 
 
-
     return (
         <div>
+            <div>
+                NLP Processor
+            </div>
+            <div>
+                <LineChart title='NLP Score' xAxis={[1, 2, 3, 5, 8, 10] } data={[2, 5.5, 2, 8.5, 1.5, 5]} />
+            </div>
             <div>
                 MRN: {selectedPatient.mrn}
             </div>
