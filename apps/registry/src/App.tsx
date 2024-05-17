71b5a02a75ab84abb7fad4d0d79794d1f50c70e9
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-05-17 11:50:03 -0700

    init app auth

--- apps/registry/src/App.tsx
+++ apps/registry/src/App.tsx
@@ -1,35 +1,127 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react';
+import { useEffect, useState } from 'react';
+import { Amplify } from 'aws-amplify';
+import { ThemeProvider } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
+import './App.css';
+import {
+  withAuthenticator,
+  useAuthenticator,
+} from '@aws-amplify/ui-react';
 
-function App() {
-  const [count, setCount] = useState(0)
+
+
+debugger;
+Amplify.configure({
+  Auth: {
+    Cognito: {
+      userPoolClientId: import.meta.env.VITE_AWS_COGNITO_APP_CLIENT_ID,
+      userPoolId: import.meta.env.VITE_AWS_COGNITO_USERPOOL_ID,
+      loginWith: {
+        oauth: {
+          domain: import.meta.env.VITE_AWS_COGNITO_OAUTH_DOMAIN,
+          providers: ['Google'],
+          scopes: ["phone", "email", "profile", "openid"],
+          responseType: 'code',
+          redirectSignIn: [
+            import.meta.env.VITE_AWS_COGNITO_SIGNIN_URL,
+          ],
+          redirectSignOut: [
+            import.meta.env.VITE_AWS_COGNITO_SIGNOUT_URL,
+          ],
+        },
+        email: true,
+      }
+    }
+  }
+});
+
+
+const theme = {
+  name: 'custom-theme',
+  overrides: [
+    {
+      colorMode: 'dark',
+      tokens: {
+        components: {
+          authenticator: {
+            modal: {
+              background: {
+                color: 'rgba(28,25,23,1)',
+              }
+            }
+          },
+          tabs: {
+            item: {
+              active: {
+                color: '{colors.black}',
+                border: {
+                  color: '{colors.black}',
+                }
+              }
+            }
+          },
+          button: {
+            primary: {
+              background: {
+                color: '{colors.black}',
+              }
+            }
+          }
+        },
+      },
+    },
+  ],
+};
+
+const App = () => {
+  const [themeMode, setThemeMode] = useState('');
+  const { authStatus } = useAuthenticator(context => [context.authStatus]);
+
+  useEffect(() => {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setThemeMode('dark');
+    }
+
+    const handleChangeTheme = (event: MediaQueryListEvent) => {
+      setThemeMode(event.matches ? "dark" : "light");
+    };
+
+    const mediaQueryList = window.matchMedia('(prefers-color-scheme: dark)');
+    mediaQueryList.addEventListener('change', handleChangeTheme);
+
+    return () => {
+      mediaQueryList.removeEventListener('change', handleChangeTheme);
+    };
+  }, []);
+
+  if(authStatus === 'configuring') {
+    return <></>
+  }
+
+  if(authStatus !== 'authenticated') {
+    return <>not authenticated</>
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <ThemeProvider theme={theme} colorMode={themeMode}>
+      <div className="App">
+        <header className="App-header">
+          Test
+        </header>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </ThemeProvider>
+  );
+};
+
+export default withAuthenticator(App);
+
+export async function getStaticProps() {
+  return {
+    props: {
+      isPassedToWithAuthenticator: true,
+    },
+  };
 }
 
-export default App
+
--- apps/registry/src/main.tsx
+++ apps/registry/src/main.tsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { Authenticator } from '@aws-amplify/ui-react';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Authenticator.Provider>
+      <App isPassedToWithAuthenticator={true} />
+    </Authenticator.Provider>
+
   </React.StrictMode>,
 )
