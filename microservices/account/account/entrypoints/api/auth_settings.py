2cca55b0d9086308a3fae47c4b42734b8dc96dd8
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-05-22 19:11:44 -0700

    cleaning up

--- microservices/account/account/entrypoints/api/auth_settings.py
+++ microservices/account/account/entrypoints/api/auth_settings.py
@@ -1,20 +1,20 @@
+import os
 from pydantic_settings import BaseSettings
 from pydantic.types import Any
+
+AWS_COGNITO_APP_CLIENT_ID = os.getenv('AWS_COGNITO_APP_CLIENT_ID')
+AWS_COGNITO_USERPOOL_ID = os.getenv('AWS_COGNITO_USERPOOL_ID')
+AWS_REGION = os.getenv('AWS_REGION')
 
 class Settings(BaseSettings):
     check_expiration: bool = True
     jwt_header_prefix: str = "Bearer"
     jwt_header_name: str = "Authorization"
     userpools: dict[str, dict[str, Any]] = {
-        "eu": {
-            "region": "USERPOOL_REGION",
-            "userpool_id": "USERPOOL_ID",
-            "app_client_id": ["APP_CLIENT_ID_1", "APP_CLIENT_ID_2"] # Example with multiple ids
-        },
         "us": {
-            "region": "USERPOOL_REGION",
-            "userpool_id": "USERPOOL_ID",
-            "app_client_id": "APP_CLIENT_ID"
+            "region": AWS_REGION,
+            "userpool_id": AWS_COGNITO_USERPOOL_ID,
+            "app_client_id": [AWS_COGNITO_APP_CLIENT_ID]
         },
     }
 
