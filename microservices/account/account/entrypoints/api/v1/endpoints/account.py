27b58cf855191922162a3d693221459e45ab0673
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-06-09 07:03:36 -0400

    + fixing env file for app docker container

--- microservices/account/account/entrypoints/api/v1/endpoints/account.py
+++ microservices/account/account/entrypoints/api/v1/endpoints/account.py
@@ -8,6 +8,7 @@ from account.entrypoints.api.config import cognito_us
 router = APIRouter()
 app_config = config.AppConfig(**config.config)
 
-@router.get("/")
-async def hello_world(auth: CognitoToken = Depends(cognito_us.auth_required)):
-    pass
+@router.get("/{account_id}")
+async def get_account(account_id: str, auth: CognitoToken = Depends(cognito_us.auth_required)):
+    #TODO: need to verify the user has access to this account
+    print("Account ID: ", account_id)
