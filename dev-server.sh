5e50a0455aad30fd8c41151869931ea1a52ed108
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-10-08 19:05:03 -0700

    + wip Kafka publish and process

--- dev-server.sh
+++ dev-server.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-
+# Create networks if they don't already exist
 docker network inspect temporal-network >/dev/null 2>&1 || \
     docker network create --driver bridge temporal-network
 
+    
 docker network inspect app_network >/dev/null 2>&1 || \
     docker network create --driver bridge app_network
-
-
+    
+# Run docker-compose with --build and --force-recreate to rebuild and recreate containers
 docker-compose \
     -f environments/dev/networks.yml \
     -f environments/dev/docker-compose-apache-kafka.yml  \
@@ -15,4 +16,5 @@ docker-compose \
     -f environments/dev/docker-compose-temporal.yml  \
     -f environments/dev/docker-compose-supertokens.yml  \
     -f environments/dev/docker-compose-neuro-service.yml  \
-    up --force-recreate
+    -p akello \
+    up --build --force-recreate
