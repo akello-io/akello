1d2ebfe45893a7a01c07d97bf34dba0394fb7ce6
Vijay Selvaraj <vijay.selvaraj@gmail.com>
2024-10-23 21:28:37 -0700

    updates

--- www/src/components/Footer.tsx
+++ www/src/components/Footer.tsx
@@ -114,18 +114,15 @@ export function Footer() {
   return (
     <Container as="footer" className="mt-24 w-full sm:mt-32 lg:mt-40">
       <FadeIn>
-        <div className="grid grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-2">
-          <Navigation />
-          <div className="flex lg:justify-end">
-            <NewsletterForm />
-          </div>
-        </div>
+
         <div className="mb-20 mt-24 flex flex-wrap items-end justify-between gap-x-6 gap-y-4 border-t border-neutral-950/10 pt-12">
           <Link href="/" aria-label="Home">
             <Logo className="h-8" fillOnHover />
           </Link>
           <p className="text-sm text-neutral-700">
-            © Studio Agency Inc. {new Date().getFullYear()}
+            {/*
+            © Akello Health Inc. {new Date().getFullYear()}
+            */}
           </p>
         </div>
       </FadeIn>
